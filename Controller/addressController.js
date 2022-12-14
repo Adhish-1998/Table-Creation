2d6141262b8d025baf34bd7b6dee341e0147e7f2
Adhish Basak <adhishbasak98@gmail.com>
2022-12-14 15:50:20 +0530

    Modified

--- Controller/addressController.js
+++ Controller/addressController.js
@@ -1,8 +1,14 @@
 const addressModel = require("../Models/addressModel")
 
 const insertAddress = async(req,res) =>{
-    let data = await addressModel.create(req.body)
+    try{
+        let data = await addressModel.create(req.body)
     return res.status(201).send({status : true, data : data})
+    }
+    catch (err){
+        return res.status(500).send({ status: false, message: err.message })
+    }
+    
 }
 
 module.exports.insertAddress = insertAddress
--- Controller/customerController.js
+++ Controller/customerController.js
@@ -1,47 +1,78 @@
 const customerModel = require("../Models/customerModel")
 
-const getCustomerList = async (req,res) =>{
-    let customerList = await customerModel.find({ isDeleted: false})
-    return res.status(200).send({status: true, data: customerList})
+const getCustomerList = async (req, res) => {
+    try {
+        let customerList = await customerModel.find({ isDeleted: false })
+        return res.status(200).send({ status: true, data: customerList })
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+
 }
 
-const getCustomer = async (req,res) =>{
-    let customer = await customerModel.findOne({ _id: req.body.id, isDeleted: false})
-    return res.status(200).send({status: true, data: customer})
+const getCustomer = async (req, res) => {
+
+    try {
+        let customer = await customerModel.findOne({ _id: req.body.id, isDeleted: false })
+        return res.status(200).send({ status: true, data: customer })
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+
 }
 
-const insertCustomer = async (req,res) =>{
-    let customer = await customerModel.create(req.body)
-    return res.status(201).send({status : true, data : customer})
+const insertCustomer = async (req, res) => {
+    try {
+        let customer = await customerModel.create(req.body)
+        return res.status(201).send({ status: true, data: customer })
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+
 }
 
-const updateCustomer = async (req,res) =>{
-    let updateObj = {}
-    if(req.body.firstName) updateObj['firstName'] = req.body.firstName
-    if(req.body.lastName) updateObj['lastName'] = req.body.lastName
-    if(req.body.dob) updateObj['dob'] = req.body.dob
-    if(req.body.gender) updateObj['gender'] = req.body.gender
-    if(req.body.password) updateObj['password'] = req.body.password
+const updateCustomer = async (req, res) => {
+    try {
+        let updateObj = {}
+        if (req.body.firstName) updateObj['firstName'] = req.body.firstName
+        if (req.body.lastName) updateObj['lastName'] = req.body.lastName
+        if (req.body.dob) updateObj['dob'] = req.body.dob
+        if (req.body.gender) updateObj['gender'] = req.body.gender
+        if (req.body.password) updateObj['password'] = req.body.password
 
-    let updatedCustomer = await customerModel.findOneAndUpdate(
-        {_id : req.params.id},
-        {updateObj},
-        {new : true}
-    )
+        let updatedCustomer = await customerModel.findOneAndUpdate(
+            { _id: req.params.id },
+            { updateObj },
+            { new: true }
+        )
 
-    return res.status(200).send({status : true, data : updatedCustomer})
+        return res.status(200).send({ status: true, data: updatedCustomer })
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+
 }
 
-const deleteCustomer = async (req,res) =>{
-    let customer = await customerModel.findOneAndUpdate(
-        {_id: req.body.id, isDeleted : false},
-        {$set: {isDeleted : true}}
-    )
-    if(!customer) return res.status(404).send({status: false, message : "Customer Details Not Found"})
-    return res.status(200).send({status : true, message : "Successfully Deleted !!!"})
+const deleteCustomer = async (req, res) => {
+    try {
+        let customer = await customerModel.findOneAndUpdate(
+            { _id: req.body.id, isDeleted: false },
+            { $set: { isDeleted: true } }
+        )
+        if (!customer) return res.status(404).send({ status: false, message: "Customer Details Not Found" })
+        return res.status(200).send({ status: true, message: "Successfully Deleted !!!" })
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+
 }
 
-module.exports ={
+module.exports = {
     getCustomerList,
     getCustomer,
     insertCustomer,
